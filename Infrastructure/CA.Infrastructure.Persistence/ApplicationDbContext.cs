c5134a8b8ae8fc2f340b6ebeea82e7fa0602e88b
onurtekin <onurtekin314@gmail.com>
2022-03-07 10:37:57 +0300

    ef migration eklendi

--- Infrastructure/CA.Infrastructure.Persistence/ApplicationDbContext.cs
+++ Infrastructure/CA.Infrastructure.Persistence/ApplicationDbContext.cs
@@ -5,12 +5,12 @@ namespace CA.Infrastructure.Persistence
 {
     public class ApplicationDbContext : DbContext
     {
-        public virtual DbSet<User> Users { get; set; }
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
         {
             
         }
+
+        public DbSet<User> Users { get; set; }
     }
 }
